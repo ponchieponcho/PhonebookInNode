98dab0baa76c57104653509070c778cefaf7f1fd
Aaron G <aaronhgross@gmail.com>
2018-03-22 14:15:56 -0400

    adding front end

--- PhonebookApp_webserver.js
+++ PhonebookApp_webserver.js
@@ -29,28 +29,6 @@ var writeContacts = function (content) {
         })
 }
 
-
-// var server = http.createServer(function (request, response) {
-//     console.log(request.method, request.url)
-
-//     if (request.method === "GET") {
-//        readContacts(
-//            function (data) {
-//                response.end(JSON.stringify(data))
-//            }
-//     )
-
-//     } else if (request.method === "POST") {
-//         
-
-//     } else if (request.method === "PUT") {
-//         console.log('Put')
-//     } else if (request.method === "DELETE") {
-//         console.log('Delete')
-//     }
-// }).listen(5000);
-
-
 var getContacts = function (request, response, params) {
     readContacts(
         function (data) {
@@ -85,21 +63,38 @@ var notFound = function (request, response) {
     response.statusCode = 404;
     response.end('404 error')
 }
+
+// let renderContact = contact => { //templating
+//     return `<li>${contact.firstName}</li>`
+// }
+// let renderJavascript = function(request, response) {
+//     fs.readFile('static/index.js', (err,data) => {
+//         response.end(data)
+//     })
+// }
+
+let renderHomepage = function(request,response) {
+    console.log('URL: '+request.url)
+        fs.readFile(`${request.url}`, (err,data) => {
+                    response.end(data)
+                })
+    }
+
 let routes = [
     { method: 'GET', path: /^\/contacts\/([0-9]+)$/, handler: getContact },
     { method: 'POST', path: /^\/contacts\/?$/, handler: postContact },
     { method: 'PUT', path: /^\/contacts\/([0-9]+)$/, handler: updateContact },
     { method: 'DELETE', path: /^\/contacts\/?$/, handler: deleteContact },
-    { method: 'GET', path: /^\/contacts\/?$/, handler: getContacts }
+    { method: 'GET', path: /^\/contacts\/?$/, handler: getContacts },
+    { method: 'GET', path: /^\/([0-9a-zA-Z -]+)?/, handler: renderHomepage }
 ];
 
 let server = http.createServer((request, response) => {
-    console.log(request.method + ' ' + request.url);
+    // console.log(request.method + ' ' + request.url);
     let params = [];
     let matchedRoute;
     for (let route of routes) {
         let match = matches(request, route.method, route.path);
-        console.log(match)
         if (match) {
             matchedRoute = route;
             params = match;
@@ -107,81 +102,4 @@ let server = http.createServer((request, response) => {
         }
     }
     (matchedRoute ? matchedRoute.handler : notFound)(request, response, params)
-}).listen(5000);;
-
-var menu = (`
-=====================
-Electronic Phone Book
-=====================
-1. Look up an entry
-2. Set an entry
-3. Delete an entry
-4. List all entries
-5. Save entries
-6. Restore saved entries
-7. Quit`);
-
-var mainMenu = function () {
-    console.log(menu)
-    // console.log(phoneBook)
-
-    rl.question('What do you want to do (1-7)? ', function (answer) {
-        if (answer === '1') { //working
-            rl.question('Name: ', function (name) {
-                for (var i = 0; i < phoneBook.length; i++) {
-                    if (phoneBook[i].firstName === name) {
-                        console.log('Found entry for ' + name + ': ' + phoneBook[i].phoneNumber);
-                        mainMenu()
-                    }
-                }
-            })
-        }
-
-        else if (answer === '2') {
-            rl.question('Name: ', function (name) {
-                rl.question('Phone Number: ', function (phone) {
-                    // phoneBook.push({ 'firstName': name, 'phoneNumber': phone });
-                    // console.log('Entry stored for ' + name);
-                    let newContact = { 'firstName': name, 'phoneNumber': phone }
-                    request.post({
-                        url: 'http://localhost/5000',
-                        body: JSON.stringify(newContact)
-                    }, function (error, response, body) {
-                        console.log(body);
-                    });
-                    mainMenu()
-                });
-            })
-        }
-
-        else if (answer === '3') { //Delete an entry
-            console.log('working')
-
-        } else if (answer === '4') { //List all entries
-            for (var i = 0; i < phoneBook.length; i++) {
-                console.log('Found entry for ' + phoneBook[i].firstName + ': ' + phoneBook[i].phoneNumber)
-            }
-            mainMenu()
-        } else if (answer === '5') { //Save entries
-            fs.writeFile(pbFileName, JSON.stringify(phoneBook), function (err) {
-                if (err) throw err;
-                console.log('Entries saved to ' + pbFileName)
-                mainMenu();
-            })
-
-        } else if (answer === '6') { //Restore saved entries
-            fs.readFile(pbFileName, function (err, data) {
-                var json = JSON.parse(data);
-                phoneBook = json;
-                mainMenu();
-            })
-
-        } else if (answer === '7') { //Quit
-            console.log('Bye!')
-            rl.close();
-        }
-
-    });
-}
-
-// mainMenu();
+}).listen(3000);;
